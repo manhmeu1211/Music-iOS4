3333c0275990a02a3295ae24d3a3531d45254125
Tran Viet Anh <henrytran@Trans-MacBook-Pro.local>
2025-08-04 19:34:36 +0700

    fix: delete timeout wrong in client.swift

--- IOS4MyMuzic/Platform/Networking/APIClient.swift
+++ IOS4MyMuzic/Platform/Networking/APIClient.swift
@@ -12,11 +12,7 @@ extension APIOperation where Self : APIRequest {
     func execute(completion: @escaping (Result<Model, Error>) -> Void) {
         self.printRequest()
         
-        let configuration = URLSessionConfiguration.default
-        configuration.timeoutIntervalForRequest = 15
-        configuration.timeoutIntervalForResource = 30
-        
-        let session = URLSession(configuration: configuration)
+        let session = URLSession(configuration: .default)
         let task = session.dataTask(with: self.request) { data, response, error in
             if let error = error {
                 DispatchQueue.main.async {
